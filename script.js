7f70bbd88083f6860864b3d9fede6b5f599f9ad3
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-01 13:39:37 +0300

    RPS UI added

--- script.js
+++ script.js
@@ -4,66 +4,86 @@ const SCISSORS = 2;
 
 function computerPlay () {
     let move = Math.floor(Math.random() * 3) ;
-    return move;
+    if (move == 0) return 'Rock'
+    else if (move == 1) return 'Paper'
+    else return 'Scissors';
 }
 
 let computerSelection = '';
 let playerSelection = '';
-let playerWin = 0, computerWin = 0, round = 5;
+let playerWin = 0, computerWin = 0;
 
 function playRound (computerSelection, playerSelection) {
-    playerSelection = prompt(`It's your turn`);
-    // playerSelection = toString(playerSelection);
-    playerSelection = playerSelection.toUpperCase();
-    if (playerSelection == `ROCK` && computerSelection == PAPER) {
-        computerWin += 1;
-        return(`paper beats rock. \n Computer wins.`);
+    let playerScore = document.querySelector('.human-score');
+    let computerScore = document.querySelector('.computer-score');
+    let message = document.querySelector('.message');
+
+    if (playerWin >= 4 || computerWin >= 4) {
+        playerWin = 0;
+        computerWin = 0;
+        playerScore.innerText = '0';
+        computerScore.innerText = '0';
+        message.innerText += ' Game Over!'
     }
-    else if (playerSelection == `ROCK` && computerSelection ==  ROCK) {
-        round += 1;
-        return(`It's a tie.`);
+
+    else {
+        if (playerSelection == `ROCK` && computerSelection == `Paper`) {
+            computerWin += 1;
+            message.innerText = `Computer chooses Paper. Computer Wins!`;
+            computerScore.innerText = `${computerWin}`;
+        }
+        else if (playerSelection == `ROCK` && computerSelection ==  `Rock`) {
+            message.innerText = `Computer chooses Rock. It's a tie.`;
+        }
+        else if (playerSelection == `ROCK` && computerSelection == `Scissors`) {
+            playerWin += 1;
+            message.innerText = 'Computer chooses Scissors. Player Wins!';
+            playerScore.innerText = `${playerWin}`;
+        }
+        else if (playerSelection == `PAPER` &&  computerSelection == `Rock`) {
+            playerWin += 1;
+            message.innerText = 'Computer chooses Rock. Player Wins!';
+            playerScore.innerText = `${playerWin}`;
+        }
+        else if (playerSelection == `PAPER` &&  computerSelection == `Paper`) {
+            message.innerText = `Computer chooses Paper. It's a tie.`;
+        }
+        else if (playerSelection == `PAPER` &&  computerSelection == `Scissors`) {
+            computerWin += 1;
+            message.innerText = `Computer chooses Scissors. Computer Wins!`;
+            computerScore.innerText = `${computerWin}`;
+        }
+        else if (playerSelection == `SCISSORS` && computerSelection == `Rock`) {
+            computerWin += 1;
+            message.innerText = `Computer chooses Rock. Computer Wins!`;
+            computerScore.innerText = `${computerWin}`; 
+        }
+        else if (playerSelection == `SCISSORS` && computerSelection == `Paper`) {
+            playerWin += 1;
+            message.innerText =`Computer chooses Paper. Player Wins!`;
+            playerScore.innerText = `${playerWin}`;
+        }
+        else if (playerSelection == `SCISSORS` && computerSelection == `Scissors`) {
+            message.innerText = `Computer chooses Scissors. It's a tie.`;
+        }
     }
-    else if (playerSelection == `ROCK` && computerSelection == SCISSORS) {
-        playerWin += 1;
-        return(`rock beats scissors. \n Player wins.`);
-    }
-    else if (playerSelection == `PAPER` &&  computerSelection == ROCK) {
-        playerWin += 1;
-        return(`paper beats rock. \n Player wins.`);
-    }
-    else if (playerSelection == `PAPER` &&  computerSelection == PAPER) {
-        round += 1;
-        return(`It's a tie.`);
-    }
-    else if (playerSelection == `PAPER` &&  computerSelection == SCISSORS) {
-        computerWin += 1;
-        return(`scissors beats paper. \n Computer wins.`);
-    }
-    else if (playerSelection == `SCISSORS` && computerSelection == ROCK) {
-        computerWin += 1;
-        return(`rock beats scissors. \n Computer wins.`);
-    }
-    else if (playerSelection == `SCISSORS` && computerSelection == PAPER) {
-        playerWin += 1;
-        return(`scissors beats paper. \n Player wins.`);
-    }
-    else if (playerSelection == `SCISSORS` && computerSelection == SCISSORS) {
-        round += 1;
-        return(`It's a tie.`);
-    }
+
 }
 
-// const playerSelection = 'ROCK';
-// const computerSelection = computerPlay();
-// console.log(playRound(computerSelection, playerSelection));
 
 function game () {
-    playerWin = 0, computerWin = 0;
-    for (let i = 0; i < round; i ++) {
-        computerSelection = computerPlay();
-        console.log(playRound(computerSelection, playerSelection));
-        console.log(`Player score ${playerWin}, Computer score ${computerWin}`);
-    }
+    let buttons = document.querySelectorAll('button');
+    let message = document.querySelector('.message');
+    let playerScore = document.querySelector('.human-score');
+    let computerScore = document.querySelector('.computer-score');
+    
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let choice = button.className;
+            choice = choice.toUpperCase();
+            playRound(computerPlay(), choice);
+        })
+    })
 }
 
 game();
